b53bdeb3357f92d7c9a94d816065d44d47c0e692
Nicolas Castelli <castellinicolas11199@gmail.com>
2025-09-25 19:28:14 +0200

    feat: added always one card open on features section + improved astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
+    prefetch: true,
+    compressHTML: true,
     vite: {
         plugins: [tailwindcss()],
       },
